2f94670126d5cd6171b2629a4fc63474a26300cd
KVEBEKER <denis2006denis18@gmail.com>
2026-09-24 18:03:02 +0500

    Add Empty Projects for Second Lab

---
+++ sem-1-lab-02-server-config/S1L02/ServerConfigProgram.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace ServerConfigProgram.Tests
+{
+    public class Tests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ sem-1-lab-02-server-config/S1L02/ServerConfigProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ServerConfigProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
